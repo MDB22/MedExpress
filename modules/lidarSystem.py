1766c1babefe627ab26f68e591826aa09335fb57
Matt De Bono <matthew.j.debono@gmail.com>
2015-07-09 23:18:29 +0100

    Added process to lidarSystem

--- modules/lidarSystem.py
+++ modules/lidarSystem.py
@@ -2,6 +2,8 @@
 # made up of two servos (pan and tilt) and LiDAR sensor
 
 import os
+import multiprocessing
+import numpy as np
 
 from servoControl import * 
 from lidar import *
@@ -19,12 +21,19 @@ PINS = '7,11'
 SERVO_PIPE = '/dev/servoblaster'
 
 # Limits for servo controls in microseconds, approximate 180o arc
-MIN_WIDTH = 600
+MIN_WIDTH = 1000
 MAX_WIDTH = 2300
 
 # Servo angle limits
 MIN_ANGLE = 0
 MAX_ANGLE = 180
+
+# Angle increment for different scan resolutions
+ANGLE_INC = 5
+
+# Resolution of scans (at 5o increments)
+NUM_POINTS_PER_PAN = 148
+NUM_POINTS_PER_TILT = 37
 
 # LiDAR poll frequency in Hz
 FREQ = 65
@@ -37,9 +46,12 @@ def map(value, in_low, in_high, out_low, out_high):
         # Convert the 0-1 range into a value in the new range.
         return out_low + valueScaled * (out_high - out_low)
 
-class LidarSystem:
+class LidarSystem(multiprocessing.Process):
 	
 	def __init__(self):
+		# Call superclass init
+		super(LidarSystem, self).__init__()
+		
 		# Need to kill the process if it is already running, or we can't
 		# create a new one or change parameters
 		os.system('sudo killall servod -q')
@@ -52,8 +64,8 @@ class LidarSystem:
 		self.pipe = open(SERVO_PIPE,'w')
 		
 		# Create servo controllers for pan and tilt
-		self.pan = ServoControl(0)
-		self.tilt = ServoControl(1)
+		self.pan = ServoControl(1)
+		self.tilt = ServoControl(0)
 		
 		# Create lidar object
 		self.lidar = Lidar(FREQ)
@@ -88,6 +100,7 @@ class LidarSystem:
 			raise IOError()
 			
 		servo.setPosition(self.pipe, width)
+			
 	
 	# Performs raster scan with LiDAR
 	def scan(self):
@@ -95,7 +108,7 @@ class LidarSystem:
 		up = True
 		
 		while True:
-			for angle in range(MIN_ANGLE, MAX_ANGLE+1):
+			for angle in range(MIN_ANGLE, MAX_ANGLE+1, ANGLE_INC):
 				# Move tilt servo
 				self.setAngle(self.tilt, angle)
 				
@@ -117,9 +130,9 @@ class LidarSystem:
 		angles = []
 		
 		if start < end:
-			angles = range(start, end, 2)
+			angles = range(start, end, ANGLE_INC)
 		else:
-			angles = range(start, end, -2)
+			angles = range(start, end, -ANGLE_INC)
 			
 		for angle in angles:
 			self.setAngle(self.pan, angle)
@@ -128,3 +141,53 @@ class LidarSystem:
 		
 		print "Length is" + str(len(data))
 		return data
+		
+	# Run method for Process, performs in-flight, low-res scan
+	def run(self):
+		# 2D array for scans, initialised to "error" value of -1
+		data = np.zeros((NUM_POINTS_PER_TILT, NUM_POINTS_PER_PAN), dtype=float) - 1
+		
+		# Control for changing direction of sweep
+		pan_direction = -1
+		tilt_direction = -1
+		
+		# Counters for indexing array
+		pan_index = 0
+		tilt_index = 0
+		
+		# Start both servos at close to 0
+		pan_angle = 0
+		tilt_angle = 0
+		
+		# Offset to get overlapping scans
+		pan_offset = 1.25
+		
+		# Initialise servos
+		self.setAngle(self.pan, pan_angle)
+		self.setAngle(self.tilt, tilt_angle)
+		data[tilt_index, pan_index] = self.lidar.getRange()
+				
+		while True:
+			# If tilt exceeds limit, reverse direction
+			if tilt_angle >= 180 or tilt_angle <= 0:
+				tilt_direction *= -1
+			
+			# If pan exceeds limit, reverse direction
+			if pan_angle >= 180 or pan_angle <= 0:
+				pan_direction *= -1
+			
+			pan_angle += pan_direction * ANGLE_INC/float(4)				
+			pan_index += pan_direction 
+				
+			tilt_angle += tilt_direction * ANGLE_INC				
+			tilt_index += tilt_direction 
+			
+			self.setAngle(self.pan, pan_angle)
+			self.setAngle(self.tilt, tilt_angle)
+			data[tilt_index, pan_index] = self.lidar.getRange()
+			
+			print "Pan at " + str(pan_angle)
+			print "Tilt at " + str(tilt_angle)
+			print "Just read: " + str(data[tilt_index, pan_index])
+			
+			
