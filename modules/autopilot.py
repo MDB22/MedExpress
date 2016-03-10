986395dcc3ddbc97f638e51247855ae394f88144
unknown <mdebono@4000D-110965-W.unimelb.edu.au>
2016-03-10 16:50:58 +1100

    Add test for arming and activating motors on real drone

--- modules/autopilot.py
+++ modules/autopilot.py
@@ -1,9 +1,10 @@
 import multiprocessing
-import dronekit
+import exceptions
 import socket
 import time
 
-#from dronekit.lib import VehicleMode, Location
+from dronekit import *
+
 #from pymavlink import mavutil
 #from time import sleep
 #from uav_logging import *
@@ -21,9 +22,9 @@ class Autopilot():
     GPS_NO_FIX = [0,1] # 0,1 = No fix, 2 = 2D fix, 3 = 3D fix
     LOC_ACCURACY = 0.95
 
-    def __init__(self, ip, mission_info, vehicle_command, log_q):
+    def __init__(self, device, mission_info, vehicle_command, log_q):
         
-        self.vehicle = self.connectToVehicle(ip)
+        self.vehicle = self.connectToVehicle(device)
         self.setHomeLocation(mission_info['base_location'][0])
         
         # Potentially incorrect ordering here, main passes tuple of (Pipe, Lock, Lock)
@@ -31,17 +32,17 @@ class Autopilot():
         self.log_q = log_q
         self.module_name = self.__class__.__name__
 
-    def connectToVehicle(self, ip):
-        print("Connecting to " + ip)
+    def connectToVehicle(self, device):
+        print("Connecting to " + device)
 
         # Connect to vehicle, be on the look out for failures
         try:
-            vehicle = dronekit.connect(ip, heartbeat_timeout=15, wait_ready=True)
+            vehicle = connect(device, heartbeat_timeout=15, wait_ready=True, baud=57600)
             return vehicle
         # Bad TCP connection
         except socket.error:
             print("No server exists!")
-        except dronekit.APIException:
+        except APIException:
             print("Timeout!")
         # Bad TTY connection
         except exceptions.OSError as e:
@@ -50,6 +51,8 @@ class Autopilot():
         except:
             print("Something else went wrong!")
 
+    # Sets the vehicle's home location to be the base
+    # position defined in the KMZ file.
     def setHomeLocation(self, location):
         cmds = self.vehicle.commands
         cmds.download()
@@ -57,75 +60,15 @@ class Autopilot():
         print(self.vehicle.home_location)
 
         # Set base location
-        self.vehicle.home_location = dronekit.LocationGlobal(location['lat'],\
-                                                             location['long'],\
-                                                             location['alt'])
+        # Can only set location if there is a GPS fix
+##        self.vehicle.home_location = LocationGlobal(location['lat'],\
+##                                                             location['long'],\
+##                                                             location['alt'])
 
         print(location)
         print(self.vehicle.home_location)
 
-    def takeoff(self, altitude):
-        """ fly to altitude
-        :param altitude: int (in meters)
-        :return: boolean (reached altitude)
-        """
-        # convert command string
-        altitude = float(altitude)
-        if not self.vehicle.armed:
-            # TODO wrap in try
-            self.arm()
-
-        self.vehicle.commands.takeoff(altitude)
-        self.vehicle.flush()
-        # todo should catch connection lost as exception
-        while not self.api.exit:
-            print " Altitude: ", self.vehicle.location.alt
-            # if close to set point return
-            if self.vehicle.location.alt>=altitude*self.LOC_ACCURACY:
-                return True
-            time.sleep(1)
-        # TODO proper return check
-        return "SUCCESS"
-
-    def arm(self):
-        """ Arm the uav
-        :return: None
-        """
-        # TODO should check / raise exception on arm failure
-        # check we have booted and have GPS
-        # TODO exception on timeout amount
-        while self.vehicle.mode.name == "INITIALISING":
-            print "Waiting for vehicle to initialise" # TODO msg over log
-            time.sleep(1)
-        while self.vehicle.gps_0.fix_type in self.GPS_NO_FIX:
-            print "Waiting for GPS" # TODO msg over log
-            time.sleep(1)
-
-        # Make sure we are in guided mode for programmatic control
-        self.vehicle.mode = VehicleMode("GUIDED")
-        self.vehicle.armed = True
-        self.vehicle.flush()
-        # Check that arming has completed
-        while not self.vehicle.armed and not self.api.exit:
-            print " Waiting for arming..." # TODO msg over log
-            time.sleep(1)
-
-    def disarm(self):
-        """ Disarm the uav
-        :return: None
-        """
-        self.vehicle.armed = False
-        self.vehicle.flush()
-        # set to stabilize for manual control
-        self.vehicle.mode = VehicleMode("STABILIZE")
-
-    # Commands the aircraft to switch between rotor and fixed-wing flight
-    def transition(self):
-        pass
-        # Send transition command to aircraft, somehow change flight configuration
-
-        # Set Geofence on transition
-
+    # Starts the vehicle's mission, as defined by the KMZ file
     def start(self):
         while False:
             # read and execute commands off vehicle_command pipe
@@ -140,5 +83,76 @@ class Autopilot():
                     self.vc_send.send(response)
             sleep(1)
 
-        time.sleep(60)
+        # Arm the vehicle
+        self.arm()
+        self.takeoff(10)
+        self.disarm()
         self.vehicle.close()
+
+    # Arms the vehicle, but only if safe to do so
+    def arm(self):
+        """ Arm the uav
+        :return: None
+        """
+        
+        # TODO should check / raise exception on arm failure
+        # check we have booted and have GPS
+        # TODO exception on timeout amount
+        
+        # Don't try to arm until autopilot is ready
+##        while not self.vehicle.is_armable:
+##            print " Waiting for vehicle to initialise..."
+##            time.sleep(1)
+        
+##        while self.vehicle.gps_0.fix_type in self.GPS_NO_FIX:
+##            print "Waiting for GPS" # TODO msg over log
+##            time.sleep(1)
+            
+        print "Arming motors"
+        # Copter should arm in GUIDED mode
+        self.vehicle.mode = VehicleMode("GUIDED")
+        self.vehicle.armed = True    
+
+        # Confirm vehicle armed before attempting to take off
+        while not self.vehicle.armed:      
+            print " Waiting for arming..."
+            time.sleep(1)
+
+    def disarm(self):
+        """ Disarm the uav
+        :return: None
+        """
+        self.vehicle.armed = False
+        self.vehicle.flush()
+        # set to stabilize for manual control
+        self.vehicle.mode = VehicleMode("STABILIZE")
+
+    def takeoff(self, altitude):
+        """ fly to altitude
+        :param altitude: int (in meters)
+        :return: boolean (reached altitude)
+        """
+        # convert command string
+        altitude = float(altitude)
+        if not self.vehicle.armed:
+            # TODO wrap in try
+            self.arm()
+
+        self.vehicle.simple_takeoff(altitude)
+        
+        # todo should catch connection lost as exception
+##        while not self.api.exit:
+##            print " Altitude: ", self.vehicle.location.alt
+##            # if close to set point return
+##            if self.vehicle.location.alt>=altitude*self.LOC_ACCURACY:
+##                return True
+##            time.sleep(1)
+            
+        time.sleep(10)
+
+    # Commands the aircraft to switch between rotor and fixed-wing flight
+    def transition(self):
+        pass
+        # Send transition command to aircraft, somehow change flight configuration
+
+        # Set Geofence on transition
--- scripts/autonomy.py
+++ scripts/autonomy.py
@@ -2,16 +2,17 @@ from autopilot import *
 from mission_reader import *
 
 # IP address of vehicle
-ip = "127.0.0.1:14551"
+device = "127.0.0.1:14551"
+#device = "com6"
 
 # Name of KMZ file
 filename = 'sample_mission.kmz'
 
 # Process KMZ file
-kml = GenerateMission(filename)
+mission_info = GenerateMission(filename)
 
 # Connect to the autopilot
-pixhawk = Autopilot(ip, mission_info, None, None)
+pixhawk = Autopilot(device, mission_info, None, None)
 
 # Activate the processes
 pixhawk.start()
