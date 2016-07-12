fd04270317106407a6d13ec772ffd434dd51fe56
Matt De Bono <matthew.j.debono@gmail.com>
2016-07-12 16:48:21 +1000

    Add init modules in order to import modules

---
+++ modules/flight/__init__.py
@@ -0,0 +1,2 @@
+from dronekit import *
+from flight import *
---
+++ modules/sensing/__init__.py
@@ -0,0 +1,5 @@
+#from camera import *
+#from lidar import *
+#from lidarSystem import *
+#from servoControl import *
+#from ultrasonic import *
