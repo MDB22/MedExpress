f13c686fc2893b6eb236b756467bf51e6527cc9c
Matt De Bono <matthew.j.debono@gmail.com>
2016-07-12 16:48:41 +1000

    Implement modular device connection, add command line arg support

--- scripts/autonomy.py
+++ scripts/autonomy.py
@@ -1,12 +1,34 @@
-from autopilot import *
-from mission_reader import *
 
-# Address of vehicle
-# IP for local host testing via UDP
-device = "127.0.0.1:14551"
-# Connection string for communicating to PixHawk via telemetry radio
-#device = "com6"
-# Connection string for communication to PixHawk via USB
+from flight import *
+from command import *
+from sensing import *
+from ground_station import *
+from autopilot import *
+
+import argparse
+
+# Setup command line arguments
+parser = argparse.ArgumentParser(description='Executes autonomous mission on a UAV.')
+parser.add_argument('--connection-type', dest='device', required=True,
+	choices=['udp', 'telemetry', 'usb-windows', 'usb-linux', 'serial'],
+	help='Device to connect to for flight commands')
+
+args = parser.parse_args()
+device = args.device
+
+# Set connection address to communicate with vehicle
+if device == 'udp':
+	# Connection string for communication to Pixhawk via UDP
+	device_address = '127.0.0.1:14551'
+elif device == 'telemetry' or device == 'usb-windows':
+	# Connection string for communicating to Pixhawk via telemetry radio, or USB (Windows)
+	device_address = 'com6'
+elif device == 'usb-linux':
+	# Connection string for communication to Pixhawk via USB (Linux)
+	device_address = '/dev/ttyUSB0'
+else:
+	# Connection string for communication to Pixhawk via serial port (Linux)
+	device_address = '/dev/ttyAMA0'
 
 # Name of KMZ file
 filename = 'sample_mission.kmz'
@@ -15,11 +37,11 @@ filename = 'sample_mission.kmz'
 mission_info = GenerateMission(filename)
 
 # Connect to the autopilot
-pixhawk = Autopilot(device, mission_info, None, None)
+pixhawk = Autopilot(device_address, mission_info, None, None)
 
 # Activate the processes
 pixhawk.start()
 
 # About to exit script, make sure we cleanup
 
-print("Mission complete.")
+print('Mission complete.')
