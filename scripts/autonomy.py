ff108f5259973fd2079008fa5d1d0ef537a61661
Matt De Bono <matthew.j.debono@gmail.com>
2016-07-13 19:12:29 +1000

    Update main file structure

--- scripts/autonomy.py
+++ scripts/autonomy.py
@@ -7,41 +7,58 @@ from autopilot import *
 
 import argparse
 
-# Setup command line arguments
-parser = argparse.ArgumentParser(description='Executes autonomous mission on a UAV.')
-parser.add_argument('--connection-type', dest='device', required=True,
-	choices=['udp', 'telemetry', 'usb-windows', 'usb-linux', 'serial'],
-	help='Device to connect to for flight commands')
+# Launches the UAV's mission
+def run():
 
-args = parser.parse_args()
-device = args.device
+	device, mission_file = parse_args()
 
-# Set connection address to communicate with vehicle
-if device == 'udp':
-	# Connection string for communication to Pixhawk via UDP
-	device_address = '127.0.0.1:14551'
-elif device == 'telemetry' or device == 'usb-windows':
-	# Connection string for communicating to Pixhawk via telemetry radio, or USB (Windows)
-	device_address = 'com6'
-elif device == 'usb-linux':
-	# Connection string for communication to Pixhawk via USB (Linux)
-	device_address = '/dev/ttyUSB0'
-else:
-	# Connection string for communication to Pixhawk via serial port (Linux)
-	device_address = '/dev/ttyAMA0'
+	# Set connection address to communicate with vehicle
+	if device == 'udp':
+		# Connection string for communication to Pixhawk via UDP
+		device_address = '127.0.0.1:14551'
+	elif device == 'sitl':
+		# Connection string for communication with a headless SITL instance
+		device_address = 'tcp:127.0.0.1:5760'
+	elif device == 'telemetry' or device == 'usb-windows':
+		# Connection string for communicating to Pixhawk via telemetry radio, or USB (Windows)
+		device_address = 'com6'
+	elif device == 'usb-linux':
+		# Connection string for communication to Pixhawk via USB (Linux)
+		device_address = '/dev/ttyUSB0'
+	else:
+		# Connection string for communication to Pixhawk via serial port (Linux)
+		device_address = '/dev/ttyAMA0'
 
-# Name of KMZ file
-filename = 'sample_mission.kmz'
+	# Process KMZ file
+	mission_info = mission_reader.GenerateMission(mission_file)
 
-# Process KMZ file
-mission_info = GenerateMission(filename)
+	# Connect to the autopilot
+	pixhawk = Autopilot(device_address, mission_info, None, None)
 
-# Connect to the autopilot
-pixhawk = Autopilot(device_address, mission_info, None, None)
+	# Activate the processes
+	pixhawk.start()
 
-# Activate the processes
-pixhawk.start()
+	# About to exit script, make sure we cleanup
+	pixhawk.join()
 
-# About to exit script, make sure we cleanup
+	print('Mission complete.')
 
-print('Mission complete.')
+# Processes command lne arguments to configure mission execution
+def parse_args():
+	# Setup command line arguments
+	parser = argparse.ArgumentParser(description='Executes autonomous mission on a UAV.')
+	parser.add_argument('--connection-type', dest='device', required=True,
+		choices=['udp', 'sitl', 'telemetry', 'usb-windows', 'usb-linux', 'serial'],
+		help='Device to connect to for flight commands')
+	parser.add_argument('--mission-file', dest='mission', required=True,
+		help='File path to locate mission KMZ file')
+
+	args = parser.parse_args()
+
+	device = args.device
+	mission_file = args.mission
+
+	return device, mission_file
+
+if __name__ == "__main__":
+	run()
