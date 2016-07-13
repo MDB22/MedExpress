996e643034ee791c386edfe500553d1cfc96e478
Matt De Bono <matthew.j.debono@gmail.com>
2016-07-13 19:13:32 +1000

    Update execution script for new environment

--- start_uav.sh
+++ start_uav.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Move to execution directory
 cd scripts/
-mavproxy.py --master=/dev/ttyAMA0 --baudrate 57600
-cd ~/med_express_uav
+
+# Set up command line args
+CONNECTION_TYPE = "udp"
+MISSION_FILE = "sample_mission.kmz"
+
+echo $CONNECTION_TYPE
+
+# Start mavproxy
+
+# Launch mission
+python autonomy.py --connection-type $CONNECTION_TYPE --mission-file $MISSION_FILE
